10db6ba2126cbea9f6b04b3cfbb8b9a4852f61cc
Shreya Kurian <shreyakurian@Shreyas-MacBook-Pro.local>
2021-09-13 16:43:08 +0530

    Added ability to display cards

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ let root = document.querySelector("#root");
 
 let main = document.createElement("div")
 let insideMainDiv = document.createElement("div")
-
 window.onload = function () {
   document.body.appendChild(main);
 };
@@ -20,25 +19,31 @@ appHeading.innerHTML = "BLING!"
 main.appendChild(appHeading)
 appHeading.style.textAlign =  "center"
 appHeading.style.marginTop =  "30px"
-
+appHeading.style.fontSize =  "30px"
+appHeading.style.fontWeight =  "bold"
+main.style.fontFamily="Montserrat"
 let headerDiv = document.createElement("div")
 let headerOne = document.createElement("h2")
 headerOne.innerHTML = "Sizzle your Life"
+headerOne.style.fontSize =  "30px"
+headerOne.style.marginTop =  "50px"
 headerDiv.appendChild(headerOne)
 
 let headerTwo = document.createElement("h1")
 headerTwo.innerHTML = "Make a Card"
 headerDiv.appendChild(headerTwo)
-
+headerTwo.style.fontSize =  "50px"
+headerTwo.style.marginTop =  "20px"
+headerTwo.style.fontWeight =  "bold"
 headerDiv.style.marginTop = "30px"
-headerDiv.style.marginLeft = "80px"
+headerDiv.style.marginBottom = "80px"
 leftSide.appendChild(headerDiv)
 
 
 const displayImage = (text) =>{
 
   let buttonImage = document.createElement("img")
-  buttonImage.style.height = "200px"
+  buttonImage.style.height = "150px"
   let element = document.querySelector(".rightImageDiv")
 
 
@@ -48,7 +53,7 @@ const displayImage = (text) =>{
     element.removeChild(element.childNodes[0]);
   }
 
-  buttonImage.style.width = "200px"
+  buttonImage.style.width = "150px"
   //if()
 
   switch(text){
@@ -73,7 +78,6 @@ const buttonCreate = (text) =>{
 }
 
 buttonList.map((buttonName)=>{
-  console.log(buttonCreate(buttonName))
   buttonsGroup1.appendChild(buttonCreate(buttonName))
 })
 leftSide.appendChild(buttonsGroup1)
@@ -128,8 +132,29 @@ radioDiv.style.border = "solid black"
 
 leftSide.appendChild(buttonsGroup2)
 
+
+
+const showCard = (text,img) => {
+let cardDiv = document.createElement("div")
+let cardHeading = document.createElement("h1")
+let cardBody = document.createElement("div")
+cardHeading.innerHTML = text
+cardHeading.style.fontSize = "50px"
+cardHeading.style.fontWeight = "bold"
+cardHeading.style.marginBottom = "20px"
+cardBody.appendChild(img[0])
+cardDiv.appendChild(cardHeading)
+cardDiv.appendChild(cardBody)
+cardDiv.style.border = "black solid"
+cardDiv.style.marginRight = "20px"
+showCardDiv.appendChild(cardDiv)
+
+}
+
+
 let cameraDiv = document.createElement("div");
 let cameraImg = document.createElement("img");
+cameraImg.addEventListener("click",()=>{showCard(rightHeading.innerHTML,rightBody.childNodes)})
 cameraImg.src = "assets/images/camera.png"
 cameraImg.style.height = "30px"
 cameraDiv.style.width = "60px"
@@ -145,9 +170,20 @@ insideMainDiv.appendChild(leftSide)
 //insideMainDiv.style.border = "red solid"
 
 
-insideMainDiv.style.display="flex"
+insideMainDiv.style.display = "flex"
+//insideMainDiv.style.justifyContent = "center"
+leftSide.style.marginLeft="300px"
+rightSide.style.marginLeft="300px"
+rightSide.style.marginTop="100px"
 let rightHeading = document.createElement("h1")
+rightHeading.style.fontWeight = "bold"
+rightHeading.style.fontSize = "40px"
+rightHeading.style.marginTop = "-40px"
+rightHeading.style.textAlign = "center"
+
+
 let rightBody = document.createElement("div")
+rightBody.style.marginTop = "30px"
 rightBody.classList.add("rightImageDiv")
 rightSide.appendChild(rightHeading)
 rightSide.appendChild(rightBody)
@@ -155,3 +191,16 @@ insideMainDiv.appendChild(rightSide)
 
 main.appendChild(insideMainDiv)
 
+let showCardDiv = document.createElement("div")
+showCardDiv.style.display = "flex"
+showCardDiv.style.flexDirection = "row"
+showCardDiv.style.flexWrap = "wrap"
+showCardDiv.style.textAlign = "center"
+
+
+
+showCardDiv.style.marginLeft = "400px"
+showCardDiv.style.marginRight = "400px"
+showCardDiv.style.marginTop = "40px"
+
+main.appendChild(showCardDiv)
